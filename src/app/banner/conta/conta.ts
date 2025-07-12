cb593e930495304e8b9090c096ed217e2bc19988
dev-raimundos <devraimundos@outlook.com>
2025-07-12 12:50:35 -0300

    FEAT: novo componente de conta bancária

---
+++ src/app/banner/conta/conta.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-conta',
+  imports: [],
+  templateUrl: './conta.html',
+  styleUrl: './conta.css'
+})
+export class Conta {
+
+}
